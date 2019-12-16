ee3d07c14bf04fca00389b4c69967216b9e87dc2
CoolGlasses <robinandrea_2@yahoo.com>
2019-12-16 12:23:09 -0800

    created initial Pawn intialization method

--- lib/chess.rb
+++ lib/chess.rb
@@ -49,6 +49,13 @@ class ChessSet
 end
 
 class Pawn
+    attr_reader :num, :color
+    attr_accessor :status
+    def initialize(num, color)
+        @num = num
+        @color = color
+        @status = "free"
+    end
 end
 
 class Knight
