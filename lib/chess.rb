b85c0c522fdcc589d9a414943353db9169c487e5
CoolGlasses <robinandrea_2@yahoo.com>
2019-12-16 11:40:28 -0800

    add class code skeleton

---
+++ lib/chess.rb
@@ -0,0 +1,31 @@
+require "byebug"
+
+class Board
+end
+
+class ChessSet
+end
+
+class Pawn
+end
+
+class Knight
+end
+
+class Castle
+end
+
+class Rook
+end
+
+class King
+end
+
+class Queen
+end
+
+class Player
+end
+
+class Game
+end
