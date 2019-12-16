ab9132ff1cbe86b7200427e3a3fcaa9a986c6466
CoolGlasses <robinandrea_2@yahoo.com>
2019-12-16 12:18:54 -0800

    created get_pieces method within the ChessSet class

--- lib/chess.rb
+++ lib/chess.rb
@@ -16,10 +16,35 @@ end
 class ChessSet
     attr_reader :color
     attr_accessor :pieces
-    
+
     def initialize(color)
         @color = color
-        @pieces = get_pieces()
+        @pieces = get_pieces(@color)
+    end
+
+    def get_pieces(color)
+        finally = []
+
+        (1..8).each do |num|
+            finally << Pawn.new(num, color)
+        end
+
+        (1..2).each do |num|
+            finally << Castle.new(num, color)
+        end
+
+        (1..2).each do |num|
+            finally << Knight.new(num, color)
+        end
+
+        (1..2).each do |num|
+            finally << Rook.new(num, color)
+        end
+
+        finally << Queen.new(color)
+        finally << King.new(color)
+
+        return finally
     end
 end
 
