b1382033a5ab2de09d0e0d930b583a51fe781787
CoolGlasses <robinandrea_2@yahoo.com>
2019-12-16 12:09:08 -0800

    created get_player_name and get_player_color within the Player class

--- lib/chess.rb
+++ lib/chess.rb
@@ -31,7 +31,27 @@ class Player
         @name = get_player_name()
         @color = get_player_color(color)
     end
-    
+
+    def get_player_name
+        puts "Hello, what is your name?"
+        name = gets.chomp
+
+        return name
+    end
+
+    def get_player_color(color)
+        if color == nil
+            puts "What color would you like to be?"
+            color = gets.chomp
+        elsif color.downcase == "black"
+            color = "white"
+        else
+            color = "black"
+        end
+
+        return color
+    end
+
 
 
 end
