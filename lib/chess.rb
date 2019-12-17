28d9ec05f67675052ff36dafe48a10f8f9853df7
CoolGlasses <robinandrea_2@yahoo.com>
2019-12-17 10:30:33 -0800

    created checkmate instance variable of Player class

--- lib/chess.rb
+++ lib/chess.rb
@@ -260,12 +260,13 @@ end
 
 class Player
     attr_reader :name, :color
-    attr_accessor :in_check
+    attr_accessor :in_check, :checkmate
 
     def initialize(color=nil)
         @name = get_player_name()
         @color = get_player_color(color)
         @in_check = false
+        @checkmate = false
     end
 
     def get_player_name
