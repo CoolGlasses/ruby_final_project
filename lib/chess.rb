f9a638f2abb177be88ca0c5792b7a5dda1089a7a
CoolGlasses <robinandrea_2@yahoo.com>
2019-12-17 10:07:02 -0800

    created set_location method for Castle class

--- lib/chess.rb
+++ lib/chess.rb
@@ -146,7 +146,26 @@ class Castle
         @num = num
         @color = color
         @status = "free"
-        @location = nil
+        @location = set_location(@num, @color)
+    end
+
+    def set_location(num, color)
+        row = 0
+        if color == "black"
+            row = 8
+        else
+            row = 1
+        end
+
+        col = 0
+        case num
+            when 1
+                col = "a"
+            when 2
+                col = "h"
+        end
+
+        return "#{col}#{row}"
     end
 end
 
