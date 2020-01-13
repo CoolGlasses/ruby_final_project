726c429f4928924eac4b4dd363a8f9ced22eb3f4
CoolGlasses <robinandrea_2@yahoo.com>
2020-01-13 11:16:29 -0800

    complete valid moves for castle

--- lib/castle.rb
+++ lib/castle.rb
@@ -1,11 +1,13 @@
 class Castle
     attr_reader :num, :color
-    attr_accessor :status, :location
+    attr_accessor :status, :location, :valid_moves
+
     def initialize(num, color)
         @num = num
         @color = color
         @status = "free"
         @location = set_location(@num, @color)
+        @valid_moves = []
     end
 
     def set_location(num, color)
@@ -24,6 +26,23 @@ class Castle
                 col = "h"
         end
 
-        return "#{col}#{row}"
+        return "#{row}#{col}"
+    end
+
+    def next_valid_moves(board_array)
+        finally = []
+        location_coordinates = @location.split("")
+        this_col = location_coordinates[1]
+        this_row = location_coordinates[0]
+        
+        (1..9).each do |row|
+            (1..9).each do |col|
+                if board_array[row][col] == " " || (@color != board_array[row][col].color && (row != 0 || row != nil) && (col != 0 || col != nil))
+                    finally << "#{row}#{col}"
+                end
+            end
+        end        
+
+        return finally
     end
 end
