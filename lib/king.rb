2d510ec95452d5872cad5403634d4ee97b673ac0
CoolGlasses <robinandrea_2@yahoo.com>
2020-01-14 10:41:25 -0800

    created initial valid moves logic for King class

--- lib/king.rb
+++ lib/king.rb
@@ -1,11 +1,12 @@
 class King
     attr_reader :num, :color
-    attr_accessor :status, :location
+    attr_accessor :status, :location, :valid_moves
     def initialize(num, color)
         @num = num
         @color = color
         @status = "free"
-        @location = place_king(@color)
+        @location = set_location(@color)
+        @valid_moves = []
 
     end
 
@@ -14,6 +15,51 @@ class King
             return "e1"
         else
             return "e8"
+        end
+    end
+
+    def next_valid_moves(board_array)
+        finally = []
+        location_coordinates = @location.split("")
+        this_col = location_coordinates[1]
+        this_row = location_coordinates[0]
+        finally << valid_move_up(board_array, this_row, this_col)
+        finally << valid_move_left(board_array, this_row, this_col)
+        finally << valid_move_down(board_array, this_row, this_col)
+        finally << valid_move_right(board_array, this_row, this_col)
+
+        return finally
+    end
+
+    def valid_move_up(board_array, this_row, this_col)
+        next_row = this_row + 1
+
+        if next_row < 9 && @color != board_array[next_row][next_col].color
+            return "#{next_row}#{this_col}"
+        end
+    end
+
+    def valid_move_left(board_array, this_row, this_col)
+        next_col = this_col + 1
+
+        if next_col < 9 && @color != board_array[next_row][next_col].color
+            return "#{this_row}#{next_col}"
+        end
+    end
+
+    def valid_move_down(board_array, this_row, this_col)
+        next_row = this_row - 1
+
+        if next_row < 0 && @color != board_array[next_row][next_col].color
+            return "#{next_row}#{this_col}"
+        end
+    end
+
+    def valid_move_right(board_array, this_row, this_col)
+        next_col = this_col - 1
+
+        if next_col > 0 && @color != board_array[next_row][next_col].color
+            return "#{this_row}#{next_col}"
         end
     end
 
