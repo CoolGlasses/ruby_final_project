043d1069d211fbddb1ba2b2a07779b6427b6c118
CoolGlasses <robinandrea_2@yahoo.com>
2020-01-14 10:18:34 -0800

    created valid moves logic for queen by combining methods utilized for castle class and bishop class

--- lib/queen.rb
+++ lib/queen.rb
@@ -1,11 +1,13 @@
 class Queen
     attr_reader :num, :color
-    attr_accessor :status, :location
+    attr_accessor :status, :location, :valid_moves
+
     def initialize(num, color)
         @num = num
         @color = color
         @status = "free"
-        @location = place_queen(@color)
+        @location = set_location(@color)
+        @valid_moves = []
     end
 
     def set_location(color)
@@ -14,5 +16,222 @@ class Queen
         else
             return "d8"
         end
+    end
+
+    def next_valid_moves(board_array)
+        finally = []
+        location_coordinates = @location.split("")
+        this_col = location_coordinates[1]
+        this_row = location_coordinates[0]
+        finally << valid_diagonal_left_up(board_array, this_row, this_col)
+        finally << valid_diagonal_left_down(board_array, this_row, this_col)
+        finally << valid_diagonal_right_down(board_array, this_row, this_col)
+        finally << valid_diagonal_right_up(board_array, this_row, this_col)
+        finally << valid_horizontal_moves_right(board_array, this_row, this_col)
+        finally << valid_horizontal_moves_left(board_array, this_row, this_col)
+        finally << valid_vertical_moves_up(board_array, this_row, this_col)
+        finally << valid_vertical_moves_down(board_array, this_row, this_col)
+
+        return finally
+    end
+
+    def valid_diagonal_right_up(board_array, this_row, this_col)
+        finally = []
+        next_row = this_row + 1
+        next_col = this_col - 1
+
+        done = false
+        while !done
+            if next_row == 9 || next_col == 0
+                done = true
+            else
+                if board_array[next_row][next_col] == " " 
+                    finally << "#{next_row}#{next_col}"
+                    next_row += 1
+                    next_col -= 1
+                elsif @color != board_array[next_row][next_col].color
+                    done = true
+                    finally << "#{next_row}#{next_col}"
+                else
+                    done = true
+                end
+            end
+        end
+
+        return finally
+    end
+
+    def valid_diagonal_right_down(board_array, this_row, this_col)
+        finally = []
+        next_row = this_row - 1
+        next_col = this_col - 1
+
+        done = false
+        while !done
+            if next_row == 0 || next_col == 00
+                done = true
+            else
+                if board_array[next_row][next_col] == " " 
+                    finally << "#{next_row}#{next_col}"
+                    next_row -= 1
+                    next_col -= 1
+                elsif @color != board_array[next_row][next_col].color
+                    done = true
+                    finally << "#{next_row}#{next_col}"
+                else
+                    done = true
+                end
+            end
+        end
+
+        return finally
+    end
+
+    def valid_diagonal_left_up(board_array, this_row, this_col)
+        finally = []
+        next_row = this_row + 1
+        next_col = this_col + 1
+
+        done = false
+        while !done
+            if next_row == 9 || next_col == 9
+                done = true
+            else
+                if board_array[next_row][next_col] == " " 
+                    finally << "#{next_row}#{next_col}"
+                    next_row += 1
+                    next_col += 1
+                elsif @color != board_array[next_row][next_col].color
+                    done = true
+                    finally << "#{next_row}#{next_col}"
+                else
+                    done = true
+                end
+            end
+        end
+
+        return finally
+    end
+
+    def valid_diagonal_left_down(board_array, this_row, this_col)
+        finally = []
+        next_row = this_row - 1
+        next_col = this_col + 1
+
+        done = false
+        while !done
+            if next_row == 0 || next_col == 9
+                done = true
+            else
+                if board_array[next_row][next_col] == " " 
+                    finally << "#{next_row}#{next_col}"
+                    next_row -= 1
+                    next_col += 1
+                elsif @color != board_array[next_row][next_col].color
+                    done = true
+                    finally << "#{next_row}#{next_col}"
+                else
+                    done = true
+                end
+            end
+        end
+
+        return finally
+    end 
+
+    def valid_horizontal_moves_right(board_array, this_row, this_col)
+        finally = []
+
+        next_col = this_col + 1
+        done = false    
+        while !done
+            if next_col == 9
+                done = true
+            else
+                if board_array[this_row][next_col] == " "
+                    finally << "#{this_row}#{next_col}"
+                    next_col += 1
+                elsif @color != board_array[this_row][next_col].color
+                    done = true
+                    finally << "#{this_row}#{next_col}"
+                else
+                    done = true
+                end
+            end
+        end
+        
+        return finally
+    end
+
+    def valid_horizontal_moves_left(board_array, this_row, this_col)
+        finally = []
+
+        next_col = this_col - 1
+        done = false    
+        while !done
+            if next_col == 0
+                done = true
+            else
+                if board_array[this_row][next_col] == " "
+                    finally << "#{this_row}#{next_col}"
+                    next_col -= 1
+                elsif @color != board_array[this_row][next_col].color
+                    done = true
+                    finally << "#{this_row}#{next_col}"
+                else
+                    done = true
+                end
+            end
+        end
+        
+        return finally
+    end
+
+    def valid_vertical_moves_up(board_array, this_row, this_col)
+        finally = []
+
+        next_row = this_row + 1
+        done = false
+        while !done
+            if next_row == 9
+                done = true
+            else
+                if board_array[next_row][this_col] == " "
+                    finally << "#{next_row}#{this_col}"
+                    next_row += 1
+                elsif @color != board_array[next_row][this_col].color
+                    done = true
+                    finally << "#{next_row}#{this_col}"
+                else
+                    done = true
+                end
+            end
+        end
+
+        return finally
+    end
+
+    def valid_vertical_moves_down(board_array, this_row, this_col)
+        finally = []
+
+        next_row = this_row - 1
+        done = false
+        while !done
+            if next_row == 0
+                done = true
+            else
+                if board_array[next_row][this_col] == " "
+                    finally << "#{next_row}#{this_col}"
+                    next_row -= 1
+                elsif @color != board_array[next_row][this_col].color
+                    done = true
+                    finally << "#{next_row}#{this_col}"
+                else
+                    done = true
+                end
+            end
+        end
+
+        return finally
     end
 end
