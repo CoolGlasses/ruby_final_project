80144f5a19de25cf3e9b509140f575b6edf1f798
CoolGlasses <robinandrea_2@yahoo.com>
2020-01-23 09:36:47 -0800

    refactored valid moves methodology for pawn class into helper functions and implemented order of operations safeguard against method calls on non class instances

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -38,33 +38,118 @@ class Pawn
         this_row = @location[0]
         
         if @color == "white"
-            move_one = this_row + 1
-            move_two = this_row + 2
-            attack_right = [this_row + 1, this_col + 1]
-            attack_left = [this_row + 1, this_col - 1]
+            finally << white_move_one(board_array, this_row, this_col)
+            finally << white_move_two(board_array, this_row, this_col)
+            finally << white_attack_right(board_array, this_row, this_col)
+            finally << white_attack_left(board_array, this_row, this_col)
         else
-            move_one = this_row - 1
-            move_two = this_row - 2
-            attack_right = [this_row - 1, this_col + 1]
-            attack_left = [this_row - 1, this_col - 1]
+            finally << black_move_one(board_array, this_row, this_col)
+            finally << black_move_two(board_array, this_row, this_col)
+            finally << black_attack_right(board_array, this_row, this_col)
+            finally << black_attack_left(board_array, this_row, this_col)
         end
+
+        return finally
+    end
+
+    def white_move_one(board_array, this_row, this_col)
+        finally = []
+        move_one = this_row + 1
         
         if board_array[move_one][this_col] == " "
             finally << [move_one, this_col]
         end
 
-        if board_array[move_two][this_col] == " " && (this_row == 7 || this_row == 2)
-            finally << [move_two, this_col]
-        end
+        return finally
+    end
 
-        if board_array[attack_right[0]][attack_right[1]] != " " && @color != board_array[attack_right[0]][attack_right[1]].color
-            finally << attack_right
-        end
+    def white_move_two(board_array, this_row, this_col)
+        finally = []
+        move_two = this_row + 2
 
-        if board_array[attack_left[0]][attack_left[1]] != " " && @color != board_array[attack_left[0]][attack_left[1]].color
-            finally << attack_left
+        if this_row == 2
+            if board_array[move_two][this_col] == " "
+                finally << [move_two, this_col]
+            end
         end
 
         return finally
     end
+
+    def white_attack_right(board_array, this_row, this_col)
+        finally = []
+        attack_right = [this_row + 1, this_col + 1]
+
+        if board_array[attack_right[0]][attack_right[1]] != " " && (this_row + 1) < 9 && (this_col + 1) < 9
+            if @color != board_array[attack_right[0]][attack_right[1]].color
+                finally << attack_right
+            end
+        end
+
+        return finally
+    end
+
+    def white_attack_left(board_array, this_row, this_col)
+        finally = []
+        attack_left = [this_row + 1, this_col - 1]
+
+        if board_array[attack_left[0]][attack_left[1]] != " "  && (this_row + 1) < 9 && (this_col - 1) > 0
+            if @color != board_array[attack_left[0]][attack_left[1]].color
+                finally << attack_left
+            end
+        end
+
+        return finally
+    end
+
+    def black_move_one(board_array, this_row, this_col)
+        finally = []
+        move_one = this_row - 1
+
+        if board_array[move_one][this_col] == " "
+            finally << [move_one, this_col]
+        end
+
+        return finally
+    end
+
+    def black_move_two(board_array, this_row, this_col)
+        finally = []
+        move_two = this_row - 2
+
+        if this_row == 7
+            if board_array[move_two][this_col] == " "
+                finally << [move_two, this_col]
+            end
+        end
+
+        return finally
+    end
+
+    def black_attack_right(board_array, this_row, this_col)
+        finally = []
+        attack_right = [this_row - 1, this_col + 1]
+
+         if board_array[attack_right[0]][attack_right[1]] != " "  && (this_row - 1) > 0 && (this_col + 1) < 9
+            if @color != board_array[attack_right[0]][attack_right[1]].color
+                finally << attack_right
+            end
+        end
+
+        return finally
+    end
+
+    def black_attack_left(board_array, this_row, this_col)
+        finally = []
+        attack_left = [this_row - 1, this_col - 1]
+
+        if board_array[attack_left[0]][attack_left[1]] != " "  && (this_row - 1) > 0 && (this_col - 1) < 0
+            if @color != board_array[attack_left[0]][attack_left[1]].color
+                finally << attack_left
+            end
+        end
+
+        return finally
+    end
+
 end
