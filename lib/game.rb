141280e412804bd66212c7b6a37c50ed428e0e6f
CoolGlasses <robinandrea_2@yahoo.com>
2020-02-13 10:57:08 -0800

    refactored play method entirely.  Adjusted workflow to follow this pattern: establish turn, acquire valid moves, prune valid moves to only include those which correspond to player whose turn it is, get a move from the user, check the move for validity by searching for the given move in the valid moves array that was just pruned, move the piece, add move to the move history, check for check, check for checkmate by created an up to date temporary board and pruning the available moves of the temporary board for the player whose turn it will be next, then changing the turn or ending the game.

--- lib/game.rb
+++ lib/game.rb
@@ -50,26 +50,48 @@ class Game
             end
 
             player_name = @turn.name
+            puts
             print_the_board(@board)
             puts
             p "Okay #{player_name}, you're up!"
+            puts
+
+            @board.acquire_valid_moves()
+            prune_valid_moves(@board, @turn)
+
             move = get_move(turn)
-            if valid_move_check(move, @turn, @board) == false
+
+            origin = location_converter(move[0])
+            destination = location_converter(move[1])
+
+            if !@board.valid_moves.include?([origin, destination])
+            # if valid_move_check(move, @turn, @board) == false
+                puts
                 p "That is not a valid move.  Please try again."
-            elsif @game_over == false
+                puts
+            else
+                @board = move_piece(origin, destination, @board)
+            end
+
+            @board.check_check()
+
+            if checkmate_check() == true
+                @board.checkmate = true
+                @game_over = true
+                return true
+            elsif @board.check == true
+                puts
+                puts "Check!"
+                puts
+            end
+
+            if @game_over == false
                 if @turn == @player1
                     @turn = @player2
                 else
                     @turn = @player1
                 end
             end
-
-            if @board.check == true
-                puts
-                p "Check!"
-                puts
-            end
-
         end
         game_over(@board)
     end
@@ -86,35 +108,33 @@ class Game
         return finally
     end
 
-    def valid_move_check(move, player, board)
-        origin = location_converter(move[0])
-        destination = location_converter(move[1])
-        piece = board.board[origin[0].to_i][origin[1].to_i]
-        if !check_if_piece_can_move_there(piece, destination)
-            return false
-        end
+    # def valid_move_check(move, player, board)
+    #     origin = location_converter(move[0])
+    #     destination = location_converter(move[1])
+    #     piece = board.board[origin[0].to_i][origin[1].to_i]
+    #     if !check_if_piece_can_move_there(piece, destination)
+    #         return false
+    #     end
 
-        ###need to create a new instance of the chess board and place all of hte pieces on the new board in the same places as the exisitng board -- helper methods?
+    #     proposed_board = temp_board(@move_history)
+    #     proposed_board = move_piece(origin, destination, proposed_board)
 
-        proposed_board = temp_board(@move_history)
-        proposed_board = move_piece(origin, destination, proposed_board)
+    #     if board.check == true && proposed_board.check == true && board.player_in_check == proposed_board.player_in_check
+    #         p "You would still be in check!"
+    #         return false
+    #     end
 
-        if board.check == true && proposed_board.check == true && board.player_in_check == proposed_board.player_in_check
-            p "You would still be in check!"
-            return false
-        end
+    #     @move_history << [origin, destination]
+    #     @board = move_piece(origin, destination, @board)
 
-        @move_history << [origin, destination]
-        @board = move_piece(origin, destination, @board)
-
-        if checkmate_check() == true
-            @board.checkmate = true
-            @game_over = true
-            return true
-        else
-            return true
-        end
-    end
+    #     if checkmate_check() == true
+    #         @board.checkmate = true
+    #         @game_over = true
+    #         return true
+    #     else
+    #         return true
+    #     end
+    # end
 
     def place_pieces(proposed_board, move_history)      
             move_history.each do |move_pair|
@@ -173,7 +193,6 @@ class Game
         piece_to_move.location = [new_row, new_col]
         new_board.board[this_row][this_col] = " "
         new_board.acquire_valid_moves()
-        new_board.check_check()
         return new_board
     end
 
@@ -190,16 +209,25 @@ class Game
 
     ##what if the checkmate check was a pruning of the valid moves list, and then a logic check to see if the valid moves list was empty?
 
-    def prune_valid_moves(board)
+    def prune_valid_moves(board, turn)
         finally = []
 
         board.valid_moves.each do |move|
             temp_board = temp_board(@move_history)
             temp_board = move_piece(move[0], move[1], temp_board)
-            if temp_board.check == false
-                finally << move
-            elsif temp_board.player_in_check != @turn.color
-                finally << move
+            temp_board.check_check()
+            if temp_board.board[move[0][0], move[0][1]].color == turn.color
+                correct_color == true
+            else
+                correct_color == false
+            end
+
+            if correct_color == true
+                if temp_board.check == false
+                    finally << move
+                elsif temp_board.player_in_check != turn.color
+                    finally << move
+                end
             end
         end
 
@@ -207,15 +235,20 @@ class Game
     end
 
     def checkmate_check
-        @board.valid_moves.each do |move|
-            board_to_check = temp_board(@move_history)
-            board_to_check = move_piece(move[0], move[1], board_to_check)
-            if board_to_check.player_in_check != board.player_in_check || board_to_check.player_in_check == nil
-                return false
-            end
+        if @turn == @player1
+            player_to_check == @player2
+        else
+            player_to_check == @player1
         end
 
-        return true
+        board_to_check = temp_board(@move_history)
+        prune_valid_moves(board_to_check, player_to_check)
+
+        if board_to_check.valid_moves.empty?
+            return true
+        else
+            return false
+        end
     end
 
     def game_over(board)
