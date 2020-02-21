3904dfaeb3a93cd4f3c628ddef8f5ababfaf65c2
CoolGlasses <robinandrea_2@yahoo.com>
2020-02-21 09:07:00 -0800

    added an rspec test for set_location method of King class

--- spec/king_spec.rb
+++ spec/king_spec.rb
@@ -1,3 +1,39 @@
 require "rspec"
 require "king"
 
+describe King do
+    context "when placing the King on the board to start the game" do
+        describe "#set_location" do
+            it "returns the proper location array for a White King" do
+                expect(set_location("white")).to eq([1,5])
+            end
+            it "returns the proper location array for a Black King" do
+                expect(set_location("black")).to eq([8,8])
+            end
+        end
+    end
+
+    context "when setting the unicode to be displayed" do
+        
+    end
+
+    context "when determining where the King can move next" do
+        
+    end
+
+    context "when determining whether the King can move up the board" do
+        
+    end
+
+    context "when determining whether the King can move left" do
+        
+    end
+
+    context "when determining whether the King can move down the board" do
+    
+    end
+
+    context "when determining whether the King can move right" do
+    
+    end
+end
