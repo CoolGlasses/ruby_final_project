c32b5c76996d5f5b64fd57a4fe16942d70ea4ba0
CoolGlasses <robinandrea_2@yahoo.com>
2020-02-24 12:53:29 -0800

    added rspec tests for Board class to test the place_pieces method and the build_board method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,14 +1,35 @@
 require "rspec"
 require "board"
+require "chessSet"
 
 describe Board do
     subject(:board) { Board.new }
 
     context "when building the board" do
         describe "#build_board" 
+            it "creates an array of 9 rows" do
+                expect(board.board.length).to eq(9)
+            end
 
-        describe "#place_pieces"
-    end
+            it "has rows that have 9 columns" do
+                expect(board.board[0].length).to eq(9)
+            end
+
+            it "makes the second column of the first row the letter a" do
+                expect(board.board[0][1]).to eq("a")
+            end
+        end
+
+        describe "#place_pieces" do
+            it "places the White King in the correct location" do
+                expect(board.board[1][5]).to eq(board.white_set.pieces[-1])
+            end
+
+            it "places the Black Queen in the correct location" do
+                expect(board.board[8][4]).to eq(board.black_set.pieces[-2])
+            end
+        end
+    
 
     context "when checking to see if either player is in check" do
         describe "#check_check"
