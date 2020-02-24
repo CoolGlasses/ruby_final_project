b5239142864314ab80feae3d08c52510f33313e5
CoolGlasses <robinandrea_2@yahoo.com>
2020-02-24 12:39:15 -0800

    removed erroneous file.  Added skeleton for board_spec.rb tests

---
+++ spec/board_spec.rb
@@ -0,0 +1,21 @@
+require "rspec"
+require "board"
+
+describe Board do
+    subject(:board) { Board.new }
+
+    context "when building the board" do
+        describe "#build_board" 
+
+        describe "#place_pieces"
+    end
+
+    context "when checking to see if either player is in check" do
+        describe "#check_check"
+
+        describe "#black_check"
+
+        describe "#white_check"
+    end
+
+end
